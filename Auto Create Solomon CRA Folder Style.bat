13930dac4d2b0e67f16411c7c5061c20cd80611d
SolomonLee <black840706@gmail.com>
2021-05-28 13:21:59 +0800

    edit     因應 route 包裝 成 RoutePages 自動產生資料夾也跟著修改

--- Auto Create Solomon CRA Folder Style.bat
+++ Auto Create Solomon CRA Folder Style.bat
@@ -9,20 +9,25 @@ if exist src (
 	mkdir hooks
 	mkdir images
 	mkdir reducers
-	mkdir route	
 	mkdir components
+
 	cd components
 	mkdir combo
 	mkdir elements
 	mkdir pages
 	
-	cd ../..
+	cd pages
+	mkdir route	
+	
+	cd ../../..
 	
 	echo Done!
 	echo In src
-	echo 	Create apis, common, components, css, hooks, images, reducers, route& echo.
+	echo 	Create apis, common, components, css, hooks, images, reducers& echo.
 	echo In components
-	echo 	Create combo, elements, pages
+	echo 	Create combo, elements, pages& echo.
+	echo In pages
+	echo 	Create route
 ) else (
   echo Can't found folder "src"!
 )
