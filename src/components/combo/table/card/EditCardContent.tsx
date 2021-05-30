f9eb9bb3b6a55c668205bc8fca076c00756c62f0
SolomonLee <black840706@gmail.com>
2021-05-30 17:16:07 +0800

    edit     Card 修改 Content 與 Name     並讓 Card Content 支援 Markdown 顯示

---
+++ src/components/combo/table/card/EditCardContent.tsx
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import { Card } from "../../../../hooks/autoSubscribe";
+import TextareaAutosize from "react-textarea-autosize";
+import ReactMarkdown from "react-markdown";
+
+interface Props {
+    card: Card;
+    updateCardContent: () => void;
+}
+const EditCardContent = ({ card, updateCardContent }: Props): JSX.Element => {
+    const [editContent, setEditContent] = useState(card.content);
+    const [isEdit, setIsEdit] = useState(false);
+
+    const handleEditChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setEditContent(e.target.value);
+    };
+
+    const handleIsEdit = (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+        setIsEdit(!isEdit);
+    };
+
+    const handleSaveEdit = () => {
+        if (editContent !== card.content) {
+            card.content = editContent;
+            updateCardContent();
+        }
+
+        setIsEdit(false);
+    };
+
+    if (isEdit) {
+        return (
+            <>
+                <TextareaAutosize
+                    className="form-control"
+                    value={editContent}
+                    onChange={handleEditChange}
+                />
+                <div className="functions">
+                    <button
+                        className="btn btn_style1 btn-sm"
+                        onClick={handleSaveEdit}
+                    >
+                        儲存
+                    </button>
+                    <button
+                        className="btn btn_style2 btn-sm"
+                        onClick={handleIsEdit}
+                    >
+                        <i className="bi bi-x"></i>
+                    </button>
+                </div>
+            </>
+        );
+    }
+
+    return (
+        <>
+            <div className="card_content">
+                <ReactMarkdown>{card.content}</ReactMarkdown>
+            </div>
+            <button className="btn btn_style1 btn-sm" onClick={handleIsEdit}>
+                編輯
+            </button>
+        </>
+    );
+};
+
+export default EditCardContent;
---
+++ src/components/combo/table/card/EditCardName.tsx
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import { Card } from "../../../../hooks/autoSubscribe";
+import addMessage from "../../message/Message";
+
+interface Props {
+    card: Card;
+    updateCardName: () => void;
+}
+const EditCardName = ({ card, updateCardName }: Props): JSX.Element => {
+    const [editName, setEditName] = useState(card.name);
+    const [isEdit, setIsEdit] = useState(false);
+
+    const handleEditNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setEditName(e.target.value);
+    };
+
+    const handleIsEdit = (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+        setIsEdit(!isEdit);
+    };
+
+    const handleEditNameKeyPress = (
+        e: React.KeyboardEvent<HTMLInputElement>
+    ) => {
+        if (e.key === "Enter") {
+            if (editName.length === 0) {
+                addMessage("卡片名稱不得為空");
+            }
+
+            if (editName !== card.name) {
+                card.name = editName;
+                updateCardName();
+            }
+
+            setIsEdit(false);
+        }
+    };
+
+    if (isEdit) {
+        return (
+            <>
+                <input
+                    name="EditListName"
+                    type="text"
+                    className="form-control"
+                    value={editName}
+                    onChange={handleEditNameChange}
+                    onKeyPress={handleEditNameKeyPress}
+                />
+
+                <button
+                    className="btn btn_style2 btn-sm"
+                    onClick={handleIsEdit}
+                >
+                    <i className="bi bi-x"></i>
+                </button>
+            </>
+        );
+    }
+
+    return (
+        <>
+            <span className="name">{card.name}</span>
+            <button className="btn btn_style2 btn-sm" onClick={handleIsEdit}>
+                編輯
+            </button>
+        </>
+    );
+};
+
+export default EditCardName;
